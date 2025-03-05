db1595d11c0b63af4100bdf3b43796f1e930851d
dmiddleton92 <dm.middleton92@gmail.com>
2025-03-04 18:28:19 -0700

    MongoDB schema to define the structure of a blog post, graphql type defs to mirror the MongoDB schema in graphQL

---
+++ MyBlogger/server/schema.js
@@ -0,0 +1,33 @@
+const { gql } = require('apollo-server-express');
+const mongoose = require('mongoose');
+
+const blogPostSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    author: { type: String, required: true },
+    date: { type: Date, default: Date.now },
+});
+
+const BlogPost = mongoose.model('BlogPost', blogPostSchema;)
+
+const typeDefs = gql`
+    type BlogPost {
+        id: ID!
+        title: String!
+        content: String!
+        author: String!
+        date: String!
+}
+type Query {
+    posts: [BlogPost!]!
+    post(id: ID!): BlogPost
+}
+
+type Mutation {
+    createPost(title: String!, content: String!, author: String!): BlogPost!
+    updatePost(id: ID!, title: String, content: String, author: String): BlogPost
+    deletePost(id: ID!): Boolean!
+}
+    `;
+
+    
