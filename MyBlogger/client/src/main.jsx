47b6ce9d041c2e6de446de0eeeb3c7d3ff93b3f6
dmiddleton92 <dm.middleton92@gmail.com>
2025-03-05 08:14:50 -0700

    set up connection betweeen front and backend with apollo client

--- MyBlogger/client/src/main.jsx
+++ MyBlogger/client/src/main.jsx
@@ -1,7 +1,24 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import ReactDOM from 'react-dom/client'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import App from './App.jsx'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      </Routes>
+      </BrowserRouter>
+    </ApolloProvider>
+  </StrictMode>,
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
